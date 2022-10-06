d9f781dee42ea6527f7016903c0a48882cba592e
SanketBezalwar <sanketbezalwar45@gmail.com>
2022-10-06 23:27:36 +0530

    Day 4

---
+++ Employee_Wage.java
@@ -0,0 +1,10 @@
+package Employee_Wage_Computation_Day4;
+
+public class Employee_Wage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		System.out.println();
+	}
+
+}
